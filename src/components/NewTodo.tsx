4db86ae1a9994f6afd3c62beebe7e44c7a1d174d
praks0208 <praks.sawant26@gmail.com>
2024-07-11 23:30:24 +0530

    The Context API & TypeScript

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,12 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
+
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{ onAddToDo: (text:string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+
+  const todosCtx = useContext(TodosContext);
+
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -14,7 +19,7 @@ const NewTodo: React.FC<{ onAddToDo: (text:string) => void }> = (props) => {
       return;
     }
 
-    props.onAddToDo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
